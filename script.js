701396c98a0bcd889a0036bbd8fa91226f0b9fdd
Zedrik <kurtzedrikdy@gmail.com>
2024-03-21 09:45:12 +0800

    fixed medyo

--- script.js
+++ script.js
@@ -8,31 +8,31 @@ class Pokemon{
     }
 }
 let pkmList=[
-    [`Charizard`,`https://img.pokemondb.net/sprites/black-white/anim/normal/charizard.gif`, 360, 
-    [`Flamethrower`,`fire`,95,0.95]
-    [`Dragon Claw`,`dragon`,80,0.95]
-    [`Air Slash`,`flying`,75,0.85]
-    [`Slash`,`normal`,70,0.85]
+    ['Charizard','https://img.pokemondb.net/sprites/black-white/anim/normal/charizard.gif', 360, 
+    ['Flamethrower','fire',95,0.95],
+    ['Dragon Claw',`dragon`,80,0.95],
+    ['Air Slash','fly',75,0.85],
+    ['Slash','normal',70,0.85],
     ],
 
-    [`Blastoise`,`https://img.pokemondb.net/sprites/black-white/anim/normal/blastoise.gif`, 490, 
-    [`Surf`,`water`,90,0.95]
-    [`Crunch`,`normal`,80,0.95]
-    [`Ice Punch`,`ice`,75,0.95]
-    [`Flash Cannon`,`steel`,80,0.95]
+    ['Blastoise','https://img.pokemondb.net/sprites/black-white/anim/normal/blastoise.gif', 490, 
+    ['Surf','water',90,0.95],
+    ['Crunch','normal',80,0.95],
+    ['Ice Punch','ice',75,0.95],
+    ['Flash Cannon','steel',80,0.95],
     ],
 
-    [`Venusaur`,`https://img.pokemondb.net/sprites/black-white/anim/normal/venusaur.gif`, 590, 
-    [`Petal Blizzard`,`grass`,95,0.95]
-    [`Sludge Bomb`,`poison`,90,0.95]
-    [`Earthquake`,`ground`,100,0.95]
-    [`Body Slam`,`normal`,85,0.95]
+    ['Venusaur','https://img.pokemondb.net/sprites/black-white/anim/normal/venusaur.gif', 590, 
+    ['Petal Blizzard','grass',95,0.95],
+    ['Sludge Bomb','poison',90,0.95],
+    ['Earthquake','ground',100,0.95],
+    ['Body Slam','normal',85,0.95],
     ],
 ];
 let typeMatch={
     'Charizard':[['ground'],['water','rock'],['fire','grass','steel']],
     'Blastoise':[['water'],['grass'],['fire','water']],
-    'Venusaur':[['poison'],['fire','flying','ice','steel'],['grass','water']],
+    'Venusaur':[['poison'],['fire','fly','ice','steel'],['grass','water']],
 }
 
 function spawn(bool){
@@ -41,7 +41,7 @@ function spawn(bool){
 
     if(bool){
         for(i=0;i<4;i++){
-            document.getElementById(`m`+i).value=pkm.moves[i][0];
+            document.getElementById('m'+i).value=pkm.moves[i][0];
         }
         return pkm;
     }
@@ -59,7 +59,7 @@ document.getElementById('pk2').appendChild(s2);
 document.getElementById('hp2').innerHTML=`<p>HP: `+pk2.hp+` / `+pk2.fullhp+`</p>`;
 
 for(i=0;i<4;i++){
-    let btn=document.getElementById(`m`+i);
+    let btn=document.getElementById('m'+i);
     let move=pk1.moves[i];
     function addHandler(btn,move,pk1,pk2){
         btn.addEventListener('click',function(e){
